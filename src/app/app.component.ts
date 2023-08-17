497e56f5eff8356dede520745b46ad9d0ac988d0
Remy-P78 <remy80.dev@gmail.com>
2023-08-17 13:16:57 +0200

    0.6 passage jour ouvré ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,23 +11,38 @@ export class AppComponent {
 
   present: number = 0;
   presence: number = 0;
+  presentX: number = 0;
+  presenceX: number = 0;
+  personne: number = 0;
 
   liste: string[] = [];
   calendrier: string[] = [];
+  jours1a30: number[] = [];
+  jourSemaine: string[] = [];
 
   constructor(private sharedService: SharedService) {
     this.liste = sharedService.getListe();
     this.calendrier = sharedService.getCalendrier();
+    this.jours1a30 = sharedService.getJours1a30();
+    this.jourSemaine = sharedService.getJourSemaine();
   }
 
-  maMethode(testEmission: boolean) {    
+  maMethode(j: number, testEmission: boolean) {
     this.present++;
-    this.presence = (this.present * 100) / (this.liste.length*this.calendrier.length);    
+    this.presence =
+      (this.present * 100) / (this.liste.length * this.calendrier.length);
+    this.presenceX;
+    console.log('personneIndex:', j);
   }
 
-  maMethode2(testEmission: boolean) {    
+  maMethode2(j: number, testEmission: boolean) {
     this.present--;
     this.presence =
-      (this.present * 100) / (this.liste.length * this.calendrier.length);    
+      (this.present * 100) / (this.liste.length * this.calendrier.length);
+  }
+
+  onCompteurJourOuvreChange(nouvelleValeur: number) {
+    // Traitez la nouvelle valeur ici
+    console.log('Nouvelle valeur du compteurJourOuvre:', nouvelleValeur);
   }
 }
--- src/app/components/calendrier/calendrier.component.ts
+++ src/app/components/calendrier/calendrier.component.ts
@@ -9,12 +9,16 @@ import { SharedService } from '../shared.service';
 export class CalendrierComponent {
   @Output() checkboxCochee = new EventEmitter();
   @Output() checkboxDecochee = new EventEmitter();
+  @Output() compteurJourOuvreChange = new EventEmitter<number>();
+  
   compteur: number = 0;
   coche: boolean = false;
   liste: string[] = [];
   calendrier: string[] = [];
-  jours1a30: number[] = []
-  jourSemaine: string[] = []
+  jours1a30: number[] = [];
+  jourSemaine: string[] = [];
+  conditionJourOuvre = false;
+  compteurJoursOuvre = 0;
 
   constructor(private sharedService: SharedService) {
     this.liste = sharedService.getListe();
@@ -23,21 +27,41 @@ export class CalendrierComponent {
     this.jourSemaine = sharedService.getJourSemaine();
   }
 
-  onCheck(testEmission: Event) {
-    const checkbox = testEmission.target as HTMLInputElement;
-    if (checkbox.checked) {
-      this.checkboxCochee.emit(testEmission);
-    } else {
-      this.checkboxDecochee.emit(testEmission);
+  // onCheck(testEmission: Event) {
+  //   const checkbox = testEmission.target as HTMLInputElement;
+  //   if (checkbox.checked) {
+  //     this.checkboxCochee.emit(testEmission);
+  //   } else {
+  //     this.checkboxDecochee.emit(testEmission);
+  //   }
+  // }
+  ngOnInit() {
+    for (let i = 0; i < this.jours1a30.length; i++) {
+      if (
+        (this.conditionJourOuvre = this.calendrier.includes(
+          this.jourSemaine[i % 7]
+        ))
+      ) {
+        this.compteurJoursOuvre++;
+      }
+      console.log('CJO', this.compteurJoursOuvre);
+      
     }
+    this.compteurJourOuvreChange.emit(this.compteurJoursOuvre);
   }
 
-  // onCoche() {
-  //   if (this.coche) {
-  //     this.compteur++;
-  //   } else {
-  //     this.compteur--;
-  //   }
-  //   console.log(this.compteur);
-  // }
+  onCheck(j: number, jour: string, testEmission: Event) {
+    const checkbox = testEmission.target as HTMLInputElement;
+    if (checkbox) {
+      if (checkbox.checked) {
+        this.checkboxCochee.emit({ j, jour });
+      } else {
+        this.checkboxDecochee.emit({ j, jour });
+      }
+    }
+    console.log('text', j);
+    console.log(this.liste[j]);
+  }
 }
+
+  
